f227094f7437a6949d533593afa3007f65fb5243
Paul F Sehorne <Paul@Sehorne.org>
2019-06-15 16:54:20 -0500

    Replaced MyService.h with one that is needed by both MyService.cpp and ServiceConfig.cpp

--- MyService.h
+++ MyService.h
@@ -1,8 +1,22 @@
+/*
+ * MyService.h
+ *
+ *  Created on: Apr 16, 2009
+ *      Author: Paul
+ */
+
+#ifndef MYSERVICE_H_
+#define MYSERVICE_H_
+
+
+#endif /* MYSERVICE_H_ */
+
 #define MYSERVICE "MyService"
+#define MYSERVICE_NAME "MyServiceName"
 #define MYSERVICE_DISPLAY "MyService (display)"
 #define MYSERVICE_DESCRIPTION "This is a template program for creating Services"
 #define MACHINENAME ""
 
 // Windows Service requirements
-SERVICE_STATUS          MyServiceStatus; 
-SERVICE_STATUS_HANDLE   MyServiceStatusHandle; 
+SERVICE_STATUS          MyServiceStatus;
+SERVICE_STATUS_HANDLE   MyServiceStatusHandle;
